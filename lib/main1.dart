4e70b7e6e6d3cea86a714379d0ad58e67e6b61fc
SacNinja101 <158118256+SacNinja101@users.noreply.github.com>
2025-05-30 10:41:19 -0700

    Update main1.dart

--- lib/main1.dart
+++ lib/main1.dart
@@ -1,7 +1,7 @@
 // lib/main.dart
-import 'package:flutter/material.dart';
-import 'app.dart'; // Assuming MyApp is in app.dart
+//import 'package:flutter/material.dart';
+//import 'app.dart'; // Assuming MyApp is in app.dart
 
-Future<void> main() async {
-  runApp(const MyApp());
-}
+//Future<void> main() async {
+//  runApp(const MyApp());
+//}
