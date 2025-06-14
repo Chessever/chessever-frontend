c32afc7a78a39b2bdfa49ae646cf6a8002aae681
Flutter Dev <flutterdev77@gmail.com>
2025-06-14 16:12:41 +0545

    style update
    -> Add typography

--- lib/screens/chessever_screen.dart
+++ lib/screens/chessever_screen.dart
@@ -1,10 +1,17 @@
+import 'package:chessever2/widgets/blur_background.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class ChesseverScreen extends StatelessWidget {
+class ChesseverScreen extends ConsumerWidget {
   const ChesseverScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [const BlurBackground()],
+      ),
+    );
   }
 }
---
+++ lib/utils/typography.dart
@@ -0,0 +1,216 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const String _fontFamily = 'InterDisplay';
+
+  // Display Styles
+  static const displayXlRegular = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w400,
+    fontSize: 52,
+    height: 60 / 52,
+  );
+  static const displayXlMedium = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w500,
+    fontSize: 52,
+    height: 60 / 52,
+  );
+  static const displayXlBold = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w700,
+    fontSize: 52,
+    height: 60 / 52,
+  );
+
+  static const displayLgRegular = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w400,
+    fontSize: 48,
+    height: 52 / 48,
+  );
+  static const displayLgMedium = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w500,
+    fontSize: 48,
+    height: 52 / 48,
+  );
+  static const displayLgBold = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w700,
+    fontSize: 48,
+    height: 52 / 48,
+  );
+
+  static const displayMdRegular = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w400,
+    fontSize: 36,
+    height: 44 / 36,
+  );
+  static const displayMdMedium = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w500,
+    fontSize: 36,
+    height: 44 / 36,
+  );
+  static const displayMdBold = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w700,
+    fontSize: 36,
+    height: 44 / 36,
+  );
+
+  static const displaySmRegular = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w400,
+    fontSize: 32,
+    height: 40 / 32,
+  );
+  static const displaySmMedium = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w500,
+    fontSize: 32,
+    height: 40 / 32,
+  );
+  static const displaySmBold = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w700,
+    fontSize: 32,
+    height: 40 / 32,
+  );
+
+  static const displayXsRegular = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w400,
+    fontSize: 24,
+    height: 32 / 24,
+  );
+  static const displayXsMedium = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w500,
+    fontSize: 24,
+    height: 32 / 24,
+  );
+  static const displayXsBold = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w700,
+    fontSize: 24,
+    height: 32 / 24,
+  );
+
+  // Text Styles
+  static const textXlRegular = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w400,
+    fontSize: 20,
+    height: 28 / 20,
+  );
+  static const textXlMedium = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w500,
+    fontSize: 20,
+    height: 28 / 20,
+  );
+  static const textXlBold = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w700,
+    fontSize: 20,
+    height: 28 / 20,
+  );
+
+  static const textLgRegular = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w400,
+    fontSize: 18,
+    height: 26 / 18,
+  );
+  static const textLgMedium = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w500,
+    fontSize: 18,
+    height: 26 / 18,
+  );
+  static const textLgBold = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w700,
+    fontSize: 18,
+    height: 26 / 18,
+  );
+
+  static const textMdRegular = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    height: 24 / 16,
+  );
+  static const textMdMedium = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+    height: 24 / 16,
+  );
+  static const textMdBold = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w700,
+    fontSize: 16,
+    height: 24 / 16,
+  );
+
+  static const textSmRegular = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+    height: 22 / 14,
+  );
+  static const textSmMedium = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+    height: 22 / 14,
+  );
+  static const textSmBold = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w700,
+    fontSize: 14,
+    height: 22 / 14,
+  );
+
+  static const textXsRegular = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+    height: 20 / 12,
+  );
+  static const textXsMedium = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w500,
+    fontSize: 12,
+    height: 20 / 12,
+  );
+  static const textXsBold = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w700,
+    fontSize: 12,
+    height: 20 / 12,
+  );
+
+  static const textXxsRegular = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w400,
+    fontSize: 11,
+    height: 18 / 11,
+  );
+  static const textXxsMedium = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w500,
+    fontSize: 11,
+    height: 18 / 11,
+  );
+  static const textXxsBold = TextStyle(
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w700,
+    fontSize: 11,
+    height: 18 / 11,
+  );
+}
