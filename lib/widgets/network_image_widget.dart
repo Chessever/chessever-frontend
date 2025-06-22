b17484d5a03da21b7f98fb9b43036c5a8d377fd8
Flutter Dev <flutterdev77@gmail.com>
2025-06-23 03:20:34 +0545

    update error widget

--- lib/widgets/network_image_widget.dart
+++ lib/widgets/network_image_widget.dart
@@ -1,3 +1,4 @@
+import 'package:chessever2/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class NetworkImageWidget extends StatelessWidget {
@@ -14,27 +15,56 @@ class NetworkImageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Image.network(
-      imageUrl,
+    return SizedBox(
       height: height,
-      loadingBuilder: (context, child, loadingProgress) {
-        if (loadingProgress == null) {
-          return child;
-        }
-        return Center(
-          child: CircularProgressIndicator(
-            value:
-                loadingProgress.expectedTotalBytes != null
-                    ? loadingProgress.cumulativeBytesLoaded /
-                        loadingProgress.expectedTotalBytes!
-                    : null,
-          ),
-        );
-      },
-      errorBuilder: (context, error, stackTrace) {
-        return Image.asset(placeHolder, fit: BoxFit.cover);
-      },
-      fit: BoxFit.cover,
+      child: Image.network(
+        imageUrl,
+        height: height,
+        width: double.infinity,
+        fit: BoxFit.contain,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) {
+            return child;
+          }
+          return Container(
+            height: height,
+            alignment: Alignment.center,
+            child: CircularProgressIndicator(
+              value: loadingProgress.expectedTotalBytes != null
+                  ? loadingProgress.cumulativeBytesLoaded /
+                  loadingProgress.expectedTotalBytes!
+                  : null,
+            ),
+          );
+        },
+        errorBuilder: (context, error, stackTrace) {
+          return Container(
+            height: height,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              color: kDarkGreyColor,
+            ),
+            child: Image.asset(
+              placeHolder,
+              height: height,
+              width: double.infinity,
+              fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) {
+                return Container(
+                  height: height,
+                  width: double.infinity,
+                  color: kDarkGreyColor,
+                  child: const Icon(
+                    Icons.image_not_supported,
+                    color: kWhiteColor,
+                    size: 50,
+                  ),
+                );
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }
