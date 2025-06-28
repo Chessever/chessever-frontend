abbdbaf7c2aca796b72092cc9d71b17210c134ba
Flutter Dev <flutterdev77@gmail.com>
2025-06-29 01:29:37 +0545

    Add loader

--- lib/widgets/skeleton_widget.dart
+++ lib/widgets/skeleton_widget.dart
@@ -1,20 +1,21 @@
 import 'package:chessever2/theme/app_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:shimmer/shimmer.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
-class ShimmerWidget extends StatelessWidget {
-  const ShimmerWidget({required this.child, super.key});
+class SkeletonWidget extends StatelessWidget {
+  const SkeletonWidget({required this.child, super.key});
 
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: Shimmer.fromColors(
+    return Skeletonizer(
+      ignorePointers: true,
+      effect: ShimmerEffect(
         baseColor: kBlackColor,
         highlightColor: kDarkGreyColor,
-        child: child,
       ),
+      child: child,
     );
   }
 }
