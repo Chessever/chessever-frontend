d64a408ac89982b696fbf57bb232285ce9aef7ad
Flutter Dev <flutterdev77@gmail.com>
2025-07-02 18:48:30 +0545

    Notifications and countryman
    -> Add Local Notification Setup for both Android and IOS,
    -> Add CountryMan Setup in Tour Card,
    -> Update Filter type Pop-up based on design,
    -> add permission for notifications,

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,13 +18,14 @@ plugins {
 
 android {
     namespace = "com.chessever.app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
+        isCoreLibraryDesugaringEnabled = true
+    }   
 
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
@@ -57,7 +58,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -69,7 +70,16 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+
 }
+dependencies {
+    // Latest stable Kotlin version compatible with Flutter 2025
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.0.21")
+
+    // Core library desugaring dependency
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
+}
+
 
 flutter {
     source = "../.."
