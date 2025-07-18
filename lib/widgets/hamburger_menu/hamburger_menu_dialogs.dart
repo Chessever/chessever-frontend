e3a1c75cb04ab2afe5bdb70c81146f7268de8e7e
Flutter Developer <flutterdev77@gmail.com>
2025-07-18 15:56:50 +0545

    New Design update

--- lib/widgets/hamburger_menu/hamburger_menu_dialogs.dart
+++ lib/widgets/hamburger_menu/hamburger_menu_dialogs.dart
@@ -1,24 +1,38 @@
 import 'package:chessever2/utils/responsive_helper.dart';
+import 'package:chessever2/widgets/blur_background.dart';
 import 'package:flutter/material.dart';
 import 'package:chessever2/theme/app_theme.dart';
 import 'package:chessever2/widgets/alert_dialog/alert_modal.dart';
 import 'package:chessever2/widgets/hamburger_menu/settings_dialog.dart';
 
-/// Helper class to show settings dialog from the hamburger menu
-class HamburgerMenuDialogs {
-  /// Shows the settings dialog
-  static void showSettingsDialog(BuildContext context) {
-    // First close the drawer if it's open
-    Navigator.pop(context);
+void showSettingsDialog(BuildContext context) {
+  // Close drawer if open
+  Navigator.pop(context);
 
-    // Then show the settings dialog
-    showAlertModal(
-      context: context,
-      horizontalPadding: 24.w,
-      verticalPadding: 24.h,
-      backgroundColor: kPopUpColor,
-      barrierColor: Colors.black.withOpacity(0.3),
-      child: const SettingsDialog(),
-    );
-  }
+  showModalBottomSheet(
+    context: context,
+    isScrollControlled: true,
+    // backgroundColor: Colors.transparent,
+    builder: (BuildContext bottomSheetContext) {
+      final bottomPadding = MediaQuery.of(bottomSheetContext).viewInsets.bottom;
+
+      return Padding(
+        padding: EdgeInsets.only(
+          // left: 24.w,
+          // right: 24.w,
+          bottom: bottomPadding + 24.h,
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
+          child: Container(
+            color: kPopUpColor,
+            child: IntrinsicHeight(
+              // Prevent it from expanding full height
+              child: SingleChildScrollView(child: const SettingsDialog()),
+            ),
+          ),
+        ),
+      );
+    },
+  );
 }
