38330ddbd975df12ce33b35047470310ce622748
Flutter Developer <flutterdev77@gmail.com>
2025-07-30 01:53:11 +0545

    Add Movement property
    -> Cleanup Model

--- lib/screens/chessboard/widgets/evaluation_bar_widget.dart
+++ lib/screens/chessboard/widgets/evaluation_bar_widget.dart
@@ -2,6 +2,7 @@ import 'package:chessever2/screens/chessboard/provider/chess_board_screen_provid
 import 'package:chessever2/screens/chessboard/provider/current_eval_provider.dart';
 import 'package:chessever2/theme/app_theme.dart';
 import 'package:chessever2/utils/responsive_helper.dart';
+import 'package:chessever2/widgets/skeleton_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -75,7 +76,7 @@ class EvaluationBarWidget extends ConsumerWidget {
 class EvaluationBarWidgetForGames extends ConsumerWidget {
   final double width;
   final double height;
-  final String fen; // FEN is the only thing we need
+  final String fen;
 
   const EvaluationBarWidgetForGames({
     required this.width,
@@ -86,57 +87,109 @@ class EvaluationBarWidgetForGames extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final evalAsync = ref.watch(cascadeEvalProvider(fen));
+    var whiteHeight = ((1 - 0.5) / 2) * height;
+    var blackHeight = height - whiteHeight;
 
-    final evaluation = evalAsync.when(
-      loading: () => 0.0,
-      error: (_, __) => 0.0,
-      data: (cloud) {
-        final pv = cloud.pvs.firstOrNull;
-        print(pv?.cp ?? 0);
-        return pv == null ? 0.0 : (pv.cp * 10 / 100).clamp(-10, 10) / 10;
-      },
-    );
+    return ref
+        .watch(cascadeEvalProvider(fen))
+        .when(
+          loading: () {
+            return SkeletonWidget(
+              child: _Bars(
+                width: width,
+                height: height,
+                whiteHeight: whiteHeight,
+                blackHeight: blackHeight,
+              ),
+            );
+          },
+          error:
+              (_, _) => SkeletonWidget(
+                child: _Bars(
+                  width: width,
+                  height: height,
+                  whiteHeight: whiteHeight,
+                  blackHeight: blackHeight,
+                ),
+              ),
+          data: (cloud) {
+            final pv = cloud.pvs.firstOrNull;
+            if (pv == null) {
+              return SkeletonWidget(
+                child: _Bars(
+                  width: width,
+                  height: height,
+                  whiteHeight: height * 0.5,
+                  blackHeight: height * 0.5,
+                ),
+              );
+            }
 
-    // Split point between -1 and 1 mapped to 0..height
-    final whiteHeight = ((1 - evaluation) / 2) * height;
-    final blackHeight = height - whiteHeight;
+            final cpRaw = pv.cp;
 
+            final normalized = (cpRaw.clamp(-5.0, 5.0) + 5.0) / 10.0;
+
+            final whiteRatio = (normalized * 0.99).clamp(0.01, 0.99);
+
+            final blackRatio = 0.99 - whiteRatio;
+
+            return _Bars(
+              width: width,
+              height: height,
+              blackHeight: blackRatio * height,
+              whiteHeight: whiteRatio * height,
+            );
+          },
+        );
+  }
+}
+
+class _Bars extends StatelessWidget {
+  const _Bars({
+    required this.width,
+    required this.height,
+    required this.whiteHeight,
+    required this.blackHeight,
+    super.key,
+  });
+
+  final double width;
+  final double height;
+  final double whiteHeight;
+  final double blackHeight;
+
+  @override
+  Widget build(BuildContext context) {
     return SizedBox(
       width: width,
       height: height,
       child: Stack(
+        alignment: Alignment.center,
         children: [
-          // White side (top)
           Align(
             alignment: Alignment.topCenter,
             child: AnimatedContainer(
-              duration: const Duration(milliseconds: 300),
-              curve: Curves.easeInOut,
-              width: width,
-              height: whiteHeight,
-              color: kPopUpColor,
-            ),
-          ),
-          // Black side (bottom)
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: AnimatedContainer(
-              duration: const Duration(milliseconds: 300),
+              duration: const Duration(milliseconds: 200),
               curve: Curves.easeInOut,
               width: width,
               height: blackHeight,
               color: kPopUpColor,
             ),
           ),
-          // Middle marker
           Align(
-            alignment: Alignment.center,
-            child: Container(
+            alignment: Alignment.bottomCenter,
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 200),
+              curve: Curves.easeInOut,
               width: width,
-              height: 4,
-              color: kRedColor,
+              height: whiteHeight,
+              color: kWhiteColor,
             ),
+          ),
+          Container(
+            width: width,
+            height: 2,
+            color: kRedColor,
           ),
         ],
       ),
