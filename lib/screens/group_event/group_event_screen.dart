1abf42edd99d1ef1d641842a745d6240717e3d55
Flutter Developer <flutterdev77@gmail.com>
2025-09-03 01:24:27 +0545

    Cleanup Group Event

--- lib/screens/group_event/group_event_screen.dart
+++ lib/screens/group_event/group_event_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chessever2/screens/group_event/widget/all_events_tab_widget.dart';
 import 'package:chessever2/screens/home/home_screen.dart';
 import 'package:chessever2/screens/home/home_screen_provider.dart';
 import 'package:chessever2/screens/group_event/providers/group_event_screen_provider.dart';
@@ -5,7 +6,6 @@ import 'package:chessever2/screens/group_event/model/tour_event_card_model.dart'
 import 'package:chessever2/screens/group_event/providers/sorting_all_event_provider.dart';
 import 'package:chessever2/theme/app_theme.dart';
 import 'package:chessever2/utils/responsive_helper.dart';
-import 'package:chessever2/widgets/event_card/event_card.dart';
 import 'package:chessever2/widgets/event_card/starred_provider.dart';
 import 'package:chessever2/widgets/filter_popup.dart';
 import 'package:chessever2/widgets/generic_error_widget.dart';
@@ -15,7 +15,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../widgets/segmented_switcher.dart';
-import '../../widgets/event_card/completed_event_card.dart';
 
 enum GroupEventCategory { current, upcoming }
 
@@ -118,17 +117,22 @@ class GroupEventScreen extends HookConsumerWidget {
                 .when(
                   data: (filteredEvents) {
                     final favorites = ref.watch(starredProvider);
-
-                    final sortedEvents = ref
-                        .read(tournamentSortingServiceProvider)
-                        .sortBasedOnFavorite(
-                          tours: filteredEvents,
-                          favorites: favorites,
-                        );
+                    final isSearching = searchController.text.trim().isNotEmpty;
+                    List<GroupEventCardModel> finalEvents;
+                    if (isSearching) {
+                      finalEvents = filteredEvents;
+                    } else {
+                      finalEvents = ref
+                          .read(tournamentSortingServiceProvider)
+                          .sortBasedOnFavorite(
+                            tours: filteredEvents,
+                            favorites: favorites,
+                          );
+                    }
 
                     return Expanded(
                       child: AllEventsTabWidget(
-                        filteredEvents: sortedEvents,
+                        filteredEvents: finalEvents,
                         onSelect:
                             (tourEventCardModel) => ref
                                 .read(groupEventScreenProvider.notifier)
@@ -166,145 +170,6 @@ class GroupEventScreen extends HookConsumerWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class AllEventsTabWidget extends ConsumerStatefulWidget {
-  const AllEventsTabWidget({
-    required this.filteredEvents,
-    required this.onSelect,
-    super.key,
-  });
-
-  final List<GroupEventCardModel> filteredEvents;
-  final ValueChanged<GroupEventCardModel> onSelect;
-
-  @override
-  ConsumerState<AllEventsTabWidget> createState() => _AllEventsTabWidgetState();
-}
-
-class _AllEventsTabWidgetState extends ConsumerState<AllEventsTabWidget>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-
-  @override
-  void initState() {
-    super.initState();
-    _animationController = AnimationController(
-      duration: const Duration(milliseconds: 200),
-      vsync: this,
-    );
-
-    // Start animation when widget is built
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _animationController.forward();
-    });
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (widget.filteredEvents.isEmpty) {
-      return const Center(
-        child: Text(
-          'No tournaments found',
-          style: TextStyle(color: Colors.white70),
-        ),
-      );
-    }
-
-    return ListView.builder(
-      padding: EdgeInsets.only(
-        left: 20.sp,
-        right: 20.sp,
-        bottom: MediaQuery.of(context).viewPadding.bottom + 12.sp,
-      ),
-      itemCount: widget.filteredEvents.length,
-      itemBuilder: (context, index) {
-        final tourEventCardModel = widget.filteredEvents[index];
-
-        // Create staggered animation for each item
-        final itemAnimation = Tween<Offset>(
-          begin: const Offset(0, -0.5),
-          end: Offset.zero,
-        ).animate(
-          CurvedAnimation(
-            parent: _animationController,
-            curve: Interval(
-              (index * 0.1).clamp(0.0, 1.0), // Stagger start times
-              ((index * 0.1) + 0.6).clamp(0.0, 1.0), // Stagger end times
-              curve: Curves.easeOutCubic,
-            ),
-          ),
-        );
-
-        final fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
-          CurvedAnimation(
-            parent: _animationController,
-            curve: Interval(
-              (index * 0.1).clamp(0.0, 1.0),
-              ((index * 0.1) + 0.6).clamp(0.0, 1.0),
-              curve: Curves.easeOut,
-            ),
-          ),
-        );
-
-        Widget eventCard;
-        switch (tourEventCardModel.tourEventCategory) {
-          case TourEventCategory.live:
-            eventCard = EventCard(
-              tourEventCardModel: tourEventCardModel,
-              onTap: () => widget.onSelect(tourEventCardModel),
-            );
-            break;
-          case TourEventCategory.upcoming:
-            eventCard = EventCard(
-              tourEventCardModel: tourEventCardModel,
-              onTap: () => widget.onSelect(tourEventCardModel),
-            );
-            break;
-          case TourEventCategory.ongoing:
-            eventCard = EventCard(
-              tourEventCardModel: tourEventCardModel,
-              onTap: () => widget.onSelect(tourEventCardModel),
-            );
-            break;
-          case TourEventCategory.completed:
-            eventCard = CompletedEventCard(
-              tourEventCardModel: tourEventCardModel,
-              onTap: () => widget.onSelect(tourEventCardModel),
-              onDownloadTournament: () {
-                // Download tournament
-              },
-              onAddToLibrary: () {
-                // Add to library
-              },
-            );
-            break;
-        }
-
-        return AnimatedBuilder(
-          animation: _animationController,
-          builder: (context, child) {
-            return SlideTransition(
-              position: itemAnimation,
-              child: FadeTransition(
-                opacity: fadeAnimation,
-                child: Padding(
-                  padding: EdgeInsets.only(bottom: 12.sp),
-                  child: eventCard,
-                ),
-              ),
-            );
-          },
-        );
-      },
     );
   }
 }
---
+++ lib/screens/group_event/widget/all_events_tab_widget.dart
@@ -0,0 +1,145 @@
+import 'package:chessever2/screens/group_event/model/tour_event_card_model.dart';
+import 'package:chessever2/utils/responsive_helper.dart';
+import 'package:chessever2/widgets/event_card/event_card.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import '../../../widgets/event_card/completed_event_card.dart';
+
+class AllEventsTabWidget extends ConsumerStatefulWidget {
+  const AllEventsTabWidget({
+    required this.filteredEvents,
+    required this.onSelect,
+    super.key,
+  });
+
+  final List<GroupEventCardModel> filteredEvents;
+  final ValueChanged<GroupEventCardModel> onSelect;
+
+  @override
+  ConsumerState<AllEventsTabWidget> createState() => _AllEventsTabWidgetState();
+}
+
+class _AllEventsTabWidgetState extends ConsumerState<AllEventsTabWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 200),
+      vsync: this,
+    );
+
+    // Start animation when widget is built
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _animationController.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.filteredEvents.isEmpty) {
+      return const Center(
+        child: Text(
+          'No tournaments found',
+          style: TextStyle(color: Colors.white70),
+        ),
+      );
+    }
+
+    return ListView.builder(
+      padding: EdgeInsets.only(
+        left: 20.sp,
+        right: 20.sp,
+        bottom: MediaQuery.of(context).viewPadding.bottom + 12.sp,
+      ),
+      itemCount: widget.filteredEvents.length,
+      itemBuilder: (context, index) {
+        final tourEventCardModel = widget.filteredEvents[index];
+
+        // Create staggered animation for each item
+        final itemAnimation = Tween<Offset>(
+          begin: const Offset(0, -0.5),
+          end: Offset.zero,
+        ).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Interval(
+              (index * 0.1).clamp(0.0, 1.0), // Stagger start times
+              ((index * 0.1) + 0.6).clamp(0.0, 1.0), // Stagger end times
+              curve: Curves.easeOutCubic,
+            ),
+          ),
+        );
+
+        final fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Interval(
+              (index * 0.1).clamp(0.0, 1.0),
+              ((index * 0.1) + 0.6).clamp(0.0, 1.0),
+              curve: Curves.easeOut,
+            ),
+          ),
+        );
+
+        Widget eventCard;
+        switch (tourEventCardModel.tourEventCategory) {
+          case TourEventCategory.live:
+            eventCard = EventCard(
+              tourEventCardModel: tourEventCardModel,
+              onTap: () => widget.onSelect(tourEventCardModel),
+            );
+            break;
+          case TourEventCategory.upcoming:
+            eventCard = EventCard(
+              tourEventCardModel: tourEventCardModel,
+              onTap: () => widget.onSelect(tourEventCardModel),
+            );
+            break;
+          case TourEventCategory.ongoing:
+            eventCard = EventCard(
+              tourEventCardModel: tourEventCardModel,
+              onTap: () => widget.onSelect(tourEventCardModel),
+            );
+            break;
+          case TourEventCategory.completed:
+            eventCard = CompletedEventCard(
+              tourEventCardModel: tourEventCardModel,
+              onTap: () => widget.onSelect(tourEventCardModel),
+              onDownloadTournament: () {
+                // Download tournament
+              },
+              onAddToLibrary: () {
+                // Add to library
+              },
+            );
+            break;
+        }
+
+        return AnimatedBuilder(
+          animation: _animationController,
+          builder: (context, child) {
+            return SlideTransition(
+              position: itemAnimation,
+              child: FadeTransition(
+                opacity: fadeAnimation,
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: 12.sp),
+                  child: eventCard,
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
