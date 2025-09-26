925f4a403fd2ab5eb7d81ddf9b81e0d00f8b6d93
devberkay <canbr29@gmail.com>
2025-09-26 22:09:10 +0300

    further improve eval

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ Future<void> main() async {
   ]);
 
   // Load environment variables
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: ".env.dev");
   WidgetsFlutterBinding.ensureInitialized();
 
   await NotificationService.initialize();
