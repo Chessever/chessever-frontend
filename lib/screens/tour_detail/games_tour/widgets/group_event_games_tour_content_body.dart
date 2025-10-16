f2f74e1600d15a7b8c318e686f93ff04a4aa86f1
Flutter Developer <flutterdev77@gmail.com>
2025-10-16 22:24:46 +0545

    Group Event Widgets Update
    -> Create Switched View,
    -> Allow Numeric  Round Selection,
    -> Create Hide Reveal Animation,

--- lib/screens/tour_detail/games_tour/widgets/group_event_games_tour_content_body.dart
+++ lib/screens/tour_detail/games_tour/widgets/group_event_games_tour_content_body.dart
@@ -1,12 +1,13 @@
 import 'package:chessever2/screens/tour_detail/games_tour/providers/games_list_view_mode_provider.dart';
 import 'package:chessever2/screens/tour_detail/games_tour/models/games_tour_model.dart';
-import 'package:chessever2/screens/tour_detail/games_tour/providers/games_app_bar_provider.dart';
-import 'package:chessever2/screens/group_event/widget/tour_loading_widget.dart';
 import 'package:chessever2/screens/tour_detail/games_tour/widgets/group_event_match_card.dart';
-import 'package:chessever2/theme/app_theme.dart';
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:chessever2/screens/tour_detail/games_tour/providers/games_app_bar_provider.dart';
+import 'package:chessever2/screens/group_event/widget/tour_loading_widget.dart';
 import 'package:chessever2/utils/responsive_helper.dart';
+import 'package:chessever2/theme/app_theme.dart';
 import 'package:chessever2/utils/app_typography.dart';
 
 class GroupEventGamesTourContentBody extends ConsumerStatefulWidget {
@@ -28,7 +29,6 @@ class GroupEventGamesTourContentBody extends ConsumerStatefulWidget {
 
 class _GroupEventGamesTourContentBodyState
     extends ConsumerState<GroupEventGamesTourContentBody> {
-  int _selectedRoundIndex = 0;
   final ScrollController _scrollController = ScrollController();
 
   @override
@@ -39,51 +39,44 @@ class _GroupEventGamesTourContentBodyState
 
   @override
   Widget build(BuildContext context) {
-    final gamesAppBar = ref.watch(gamesAppBarProvider);
+    final selectedRoundId = ref.watch(
+      gamesAppBarProvider.select((v) => v.value?.selectedId),
+    );
 
+    final gamesAppBar = ref.watch(gamesAppBarProvider);
     if (gamesAppBar.isLoading || !gamesAppBar.hasValue) {
       return const TourLoadingWidget();
     }
-
-    final rounds =
-        ref.watch(gamesAppBarProvider).value?.gamesAppBarModels ?? [];
-
+    final rounds = gamesAppBar.value!.gamesAppBarModels;
     final gamesByRound = <String, List<GamesTourModel>>{};
-    for (final round in rounds) {
-      gamesByRound[round.id] = [];
-    }
+    for (final r in rounds) gamesByRound[r.id] = [];
 
-    for (final game in widget.gamesScreenModel.gamesTourModels) {
-      if (gamesByRound.containsKey(game.roundId)) {
-        gamesByRound[game.roundId]!.add(game);
+    for (final g in widget.gamesScreenModel.gamesTourModels) {
+      if (gamesByRound.containsKey(g.roundId)) {
+        gamesByRound[g.roundId]!.add(g);
       }
     }
 
     final visibleRounds =
-        rounds
-            .where((round) => (gamesByRound[round.id]?.isNotEmpty ?? false))
-            .toList();
-
-    if (_selectedRoundIndex >= visibleRounds.length) {
-      _selectedRoundIndex = 0;
-    }
-
+        rounds.where((r) => (gamesByRound[r.id]?.isNotEmpty ?? false)).toList();
+    final selectedIndex = visibleRounds.indexWhere(
+      (r) => r.id == selectedRoundId,
+    );
+    final safeIndex = selectedIndex < 0 ? 0 : selectedIndex;
     final selectedRound =
-        visibleRounds.isNotEmpty ? visibleRounds[_selectedRoundIndex] : null;
+        visibleRounds.isNotEmpty ? visibleRounds[safeIndex] : null;
 
     final orderedGamesForChessBoard = <GamesTourModel>[];
     final gameIndexMap = <String, int>{};
 
     int currentIndex = 0;
-    for (final round in visibleRounds) {
-      final roundGames = gamesByRound[round.id] ?? [];
-      for (final game in roundGames) {
-        gameIndexMap[game.gameId] = currentIndex;
-        orderedGamesForChessBoard.add(game);
+    for (final r in visibleRounds) {
+      for (final g in gamesByRound[r.id] ?? []) {
+        gameIndexMap[g.gameId] = currentIndex;
+        orderedGamesForChessBoard.add(g);
         currentIndex++;
       }
     }
-
     final orderedGamesData = GamesScreenModel(
       gamesTourModels: orderedGamesForChessBoard,
       pinnedGamedIs: widget.gamesScreenModel.pinnedGamedIs,
@@ -101,9 +94,9 @@ class _GroupEventGamesTourContentBodyState
                     ),
                     children: [
                       GroupEventMatchCard(
-                        key: ValueKey('round_${selectedRound.id}'),
+                        // key: ValueKey('round_${selectedRound.id}'),
                         round: selectedRound,
-                        games: gamesByRound[selectedRound.id] ?? [],
+                        games: gamesByRound[selectedRound.id]!,
                         gamesData: orderedGamesData,
                         gameIndexMap: gameIndexMap,
                         gamesListViewMode: widget.gamesListViewMode,
@@ -116,63 +109,61 @@ class _GroupEventGamesTourContentBodyState
         if (visibleRounds.isNotEmpty)
           Align(
             alignment: Alignment.bottomRight,
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.5,
-              height: 48,
-              margin: EdgeInsets.only(right: 16.sp, bottom: 16.sp),
-              child: SingleChildScrollView(
-                controller: _scrollController,
-                scrollDirection: Axis.horizontal,
-                physics: const BouncingScrollPhysics(),
-                child: Row(
-                  children: List.generate(visibleRounds.length, (i) {
-                    final index = visibleRounds.length - 1 - i;
-                    final round = visibleRounds[index];
-                    final isSelected = index == _selectedRoundIndex;
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 15),
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.5,
+                height: 48,
+                margin: EdgeInsets.only(right: 16.sp, bottom: 16.sp),
+                child: SingleChildScrollView(
+                  controller: _scrollController,
+                  scrollDirection: Axis.horizontal,
+                  physics: const BouncingScrollPhysics(),
+                  child: Row(
+                    children: List.generate(visibleRounds.length, (index) {
+                      final round = visibleRounds[index];
+                      final isSelected = round.id == selectedRoundId;
 
-                    String roundNumber = '${index + 1}';
-                    final numberMatch = RegExp(r'\d+').firstMatch(round.name);
-                    if (numberMatch != null) {
-                      roundNumber = numberMatch.group(0)!;
-                    }
+                      String roundNumber = '${index + 1}';
+                      final m = RegExp(r'\d+').firstMatch(round.name);
+                      if (m != null) roundNumber = m.group(0)!;
 
-                    return GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _selectedRoundIndex = index;
-                        });
-                      },
-                      child: AnimatedContainer(
-                        duration: const Duration(milliseconds: 200),
-                        margin: EdgeInsets.only(right: 12.sp),
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 20.sp,
-                          vertical: 12.sp,
-                        ),
-                        decoration: BoxDecoration(
-                          color: kDarkGreyColor,
-                          borderRadius: BorderRadius.circular(8),
-                          border:
-                              isSelected
-                                  ? Border.all(
-                                    color: const Color(0xFF404040),
-                                    width: 1,
-                                  )
-                                  : null,
-                        ),
-                        child: Text(
-                          roundNumber,
-                          style: AppTypography.textSmMedium.copyWith(
-                            color:
+                      return GestureDetector(
+                        onTap: () {
+                          ref.read(gamesAppBarProvider.notifier).select(round);
+                        },
+                        child: AnimatedContainer(
+                          duration: const Duration(milliseconds: 200),
+                          margin: EdgeInsets.only(right: 12.sp),
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 20.sp,
+                            vertical: 12.sp,
+                          ),
+                          decoration: BoxDecoration(
+                            color: kBlack2Color,
+                            borderRadius: BorderRadius.circular(8),
+                            border:
                                 isSelected
-                                    ? Colors.white
-                                    : const Color(0xFF666666),
-                            fontWeight: FontWeight.w500,
+                                    ? Border.all(
+                                      color: const Color(0xFF404040),
+                                      width: 1,
+                                    )
+                                    : null,
+                          ),
+                          child: Text(
+                            roundNumber,
+                            style: AppTypography.textSmMedium.copyWith(
+                              color:
+                                  isSelected
+                                      ? Colors.white
+                                      : const Color(0xFF666666),
+                              fontWeight: FontWeight.w500,
+                            ),
                           ),
                         ),
-                      ),
-                    );
-                  }),
+                      );
+                    }),
+                  ),
                 ),
               ),
             ),
