13267c1398c6e33219cea1d672a39f63e11a7ef2
devberkay <canbr29@gmail.com>
2025-10-22 18:09:57 +0300

    PRETTY GOOD

--- lib/screens/tour_detail/games_tour/widgets/group_event_games_tour_content_body.dart
+++ lib/screens/tour_detail/games_tour/widgets/group_event_games_tour_content_body.dart
@@ -3,6 +3,7 @@ import 'package:chessever2/screens/tour_detail/games_tour/providers/games_list_v
 import 'package:chessever2/screens/tour_detail/games_tour/models/games_tour_model.dart';
 import 'package:chessever2/screens/tour_detail/games_tour/widgets/games_tour_content_provider.dart';
 import 'package:chessever2/screens/tour_detail/games_tour/widgets/group_event_match_card.dart';
+import 'package:chessever2/screens/tour_detail/games_tour/widgets/round_header_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:chessever2/screens/tour_detail/games_tour/providers/games_app_bar_provider.dart';
@@ -38,15 +39,23 @@ class _GroupEventGamesTourContentBodyState
 
   @override
   Widget build(BuildContext context) {
-    final selectedRoundId = ref.watch(
-      gamesAppBarProvider.select((v) => v.value?.selectedId),
-    );
-
     final gamesAppBar = ref.watch(gamesAppBarProvider);
     if (gamesAppBar.isLoading || !gamesAppBar.hasValue) {
       return const TourLoadingWidget();
     }
     final rounds = gamesAppBar.value!.gamesAppBarModels;
+
+    // Filter to only rounds that have games
+    final visibleRounds =
+        rounds
+            .where((round) => widget.gamesScreenModel.gamesTourModels
+                .any((game) => game.roundId == round.id))
+            .toList();
+
+    if (visibleRounds.isEmpty) {
+      return const SizedBox.shrink();
+    }
+
     final orderedGamesData = ref
         .read(gamesTourContentProvider)
         .getOrderedGamesForChessBoard(
@@ -54,45 +63,67 @@ class _GroupEventGamesTourContentBodyState
           gamesScreenModel: widget.gamesScreenModel,
         );
 
-    return selectedRoundId != null
-        ? Padding(
-          padding: EdgeInsets.only(left: 16.sp, right: 16.sp, bottom: 12.sp),
-          child: _buildGroupedGameCardsBuilder(
-            rounds,
-            rounds.firstWhere((r) => r.id == selectedRoundId),
-            orderedGamesData,
-          ),
-        )
-        : const SizedBox.shrink();
+    return Padding(
+      padding: EdgeInsets.only(left: 16.sp, right: 16.sp, bottom: 12.sp),
+      child: _buildAllRoundsView(
+        visibleRounds,
+        orderedGamesData,
+      ),
+    );
   }
 
-  Widget _buildGroupedGameCardsBuilder(
-    List<GamesAppBarModel> gamesAppBarModels,
-    GamesAppBarModel selectedRound,
+  Widget _buildAllRoundsView(
+    List<GamesAppBarModel> visibleRounds,
     GamesScreenModel orderedGamesData,
   ) {
-    final grouped = ref
-        .read(gamesTourContentProvider)
-        .getGroupHeader(
-          selectedRoundId: selectedRound.id,
-          gamesScreenModel: widget.gamesScreenModel,
-        );
+    // Build a list of all items: [round header, team cards, round header, team cards, ...]
+    final allItems = <Widget>[];
 
-    // Build grouped cards
-    return ListView.builder(
-      padding: EdgeInsets.zero,
-      itemCount: grouped.length,
-      itemBuilder: (context, index) {
-        final header = grouped.keys.elementAt(index);
+    for (final round in visibleRounds) {
+      // Get team groupings for this round
+      final grouped = ref
+          .read(gamesTourContentProvider)
+          .getGroupHeader(
+            selectedRoundId: round.id,
+            gamesScreenModel: widget.gamesScreenModel,
+          );
+
+      // Get all games for this round to show in header
+      final roundGames =
+          widget.gamesScreenModel.gamesTourModels
+              .where((game) => game.roundId == round.id)
+              .toList();
+
+      // Add round header
+      allItems.add(
+        RoundHeader(
+          round: round,
+          roundGames: roundGames,
+        ),
+      );
+
+      // Add all team matchup cards for this round
+      for (final header in grouped.keys) {
         final gamesForTeam = grouped[header]!;
-
-        return GroupEventMatchCard(
-          roundTitle: header,
-          games: gamesForTeam,
-          gamesData: orderedGamesData,
-          gamesListViewMode: widget.gamesListViewMode,
-          onReturnFromChessboard: widget.onReturnFromChessboard,
+        allItems.add(
+          GroupEventMatchCard(
+            roundTitle: header,
+            games: gamesForTeam,
+            gamesData: orderedGamesData,
+            gamesListViewMode: widget.gamesListViewMode,
+            onReturnFromChessboard: widget.onReturnFromChessboard,
+          ),
         );
+      }
+    }
+
+    // Build scrollable list with all rounds
+    return ListView.builder(
+      controller: _scrollController,
+      padding: EdgeInsets.zero,
+      itemCount: allItems.length,
+      itemBuilder: (context, index) {
+        return allItems[index];
       },
     );
   }
