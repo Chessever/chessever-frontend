8a7d29debadee52748c69345a112023a8f298914
Flutter Developer <flutterdev77@gmail.com>
2025-10-28 17:28:55 +0545

    Add Loading state and Remove backdrop

--- lib/screens/premium/premium_popup_listener.dart
+++ lib/screens/premium/premium_popup_listener.dart
@@ -1,19 +1,18 @@
 import 'package:chessever2/main.dart';
 import 'package:chessever2/screens/premium/premium_screen.dart';
 import 'package:chessever2/screens/premium/provider/premiun_popup_provider.dart';
+import 'package:chessever2/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class PremiumPopupListener extends ConsumerStatefulWidget {
-  const PremiumPopupListener({
-    super.key,
-    required this.child,
-  });
+  const PremiumPopupListener({super.key, required this.child});
 
   final Widget child;
 
   @override
-  ConsumerState<PremiumPopupListener> createState() => _PremiumPopupListenerState();
+  ConsumerState<PremiumPopupListener> createState() =>
+      _PremiumPopupListenerState();
 }
 
 class _PremiumPopupListenerState extends ConsumerState<PremiumPopupListener> {
@@ -21,30 +20,28 @@ class _PremiumPopupListenerState extends ConsumerState<PremiumPopupListener> {
 
   @override
   Widget build(BuildContext context) {
-    ref.listen<PremiumPopupState>(
-      premiumPopupProvider,
-          (previous, next) {
-        final currentContext = navigatorKey.currentState?.context;
-        if (currentContext == null) return;
+    ref.listen<PremiumPopupState>(premiumPopupProvider, (previous, next) {
+      final currentContext = navigatorKey.currentState?.context;
+      if (currentContext == null) return;
 
-        if (next.isVisible && !_isBottomSheetOpen) {
-          _isBottomSheetOpen = true;
-          // Show popup when state becomes visible
-          showModalBottomSheet(
-            context: currentContext,
-            backgroundColor: Colors.transparent,
-            isScrollControlled: true,
-            builder: (_) => const PremiumScreen(),
-          ).then((_) {
-            _isBottomSheetOpen = false;
-            ref.read(premiumPopupProvider.notifier).hide();
-          });
-        } else if (!next.isVisible && _isBottomSheetOpen) {
-          Navigator.pop(currentContext);
+      if (next.isVisible && !_isBottomSheetOpen) {
+        _isBottomSheetOpen = true;
+        // Show popup when state becomes visible
+        showModalBottomSheet(
+          context: currentContext,
+          backgroundColor: Colors.transparent,
+          isScrollControlled: true,
+          barrierColor: kWhiteColor.withOpacity(0.5),
+          builder: (_) => const PremiumScreen(),
+        ).then((_) {
           _isBottomSheetOpen = false;
-        }
-      },
-    );
+          ref.read(premiumPopupProvider.notifier).hide();
+        });
+      } else if (!next.isVisible && _isBottomSheetOpen) {
+        Navigator.pop(currentContext);
+        _isBottomSheetOpen = false;
+      }
+    });
 
     return widget.child;
   }
--- lib/screens/premium/premium_screen.dart
+++ lib/screens/premium/premium_screen.dart
@@ -1,19 +1,8 @@
 import 'package:chessever2/revenue_cat_service/subscribe_state.dart';
 import 'package:chessever2/screens/premium/widget/buy_premium_card.dart';
 import 'package:chessever2/screens/premium/widget/subscription_info_card.dart';
-import 'package:chessever2/utils/extensioms/string_extensions.dart';
-import 'package:chessever2/widgets/app_button.dart';
-
-import 'package:chessever2/screens/premium/feature_row.dart';
-import 'package:chessever2/utils/app_typography.dart';
-import 'package:chessever2/widgets/back_drop_filter_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:chessever2/utils/svg_asset.dart';
-import 'package:chessever2/theme/app_theme.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:purchases_flutter/models/package_wrapper.dart';
-
-import '../../utils/get_title_by_subscription_type.dart';
 
 class PremiumScreen extends ConsumerStatefulWidget {
   const PremiumScreen({super.key});
@@ -35,24 +24,17 @@ class _PremiumScreenState extends ConsumerState<PremiumScreen> {
   @override
   Widget build(BuildContext context) {
     final subscriptionState = ref.watch(subscriptionProvider);
-    if (subscriptionState.isLoading) {
-      return Scaffold(body: Center(child: CircularProgressIndicator()));
-    }
-    // if (subscriptionState.isSubscribed) {
-    //   return Scaffold(
-    //     body: Center(
-    //       child: Text(
-    //         'Error loading subscription state',
-    //         style: AppTypography.textSmBold.copyWith(color: kBoardColorGrey),
-    //       ),
-    //     ),
-    //   );
-    // }
-    return Scaffold(
-      body: Stack(
-        alignment: Alignment.bottomCenter,
-        children: [
-          BackDropFilterWidget(),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        if (subscriptionState.isLoading) ...[
+          Card(
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height / 2,
+              child: Center(child: CircularProgressIndicator()),
+            ),
+          ),
+        ] else ...[
           if (subscriptionState.isSubscribed &&
               subscriptionState.customerInfo != null)
             SubscriptionInfoWidget(
@@ -61,7 +43,7 @@ class _PremiumScreenState extends ConsumerState<PremiumScreen> {
           else
             BuyPremiumCard(subscriptionState: subscriptionState),
         ],
-      ),
+      ],
     );
   }
 }
