d2895b7c560f6c8d26f2d19dccf144c23d2cdd3a
devberkay <canbr29@gmail.com>
2026-05-07 14:54:33 +0300

    Prevent eval bar flicker during scroll

---
+++ test/evaluation_bar_widget_test.dart
@@ -0,0 +1,84 @@
+import 'dart:async';
+
+import 'package:chessever2/repository/lichess/cloud_eval/cloud_eval.dart';
+import 'package:chessever2/screens/chessboard/provider/current_eval_provider.dart';
+import 'package:chessever2/screens/chessboard/widgets/evaluation_bar_widget.dart';
+import 'package:chessever2/screens/chessboard/widgets/player_first_row_detail_widget.dart';
+import 'package:chessever2/utils/responsive_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+const _fen = 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1';
+
+CloudEval _cloudEval(int cp) {
+  return CloudEval(
+    fen: _fen,
+    knodes: 0,
+    depth: 12,
+    pvs: [Pv(moves: 'e7e5', cp: cp)],
+    requestedMultiPv: 1,
+  );
+}
+
+Future<void> _pumpEvalBar(
+  WidgetTester tester, {
+  required bool allowStockfishFallback,
+  required Future<CloudEval> Function() cacheOnlyEval,
+}) async {
+  await tester.pumpWidget(
+    ProviderScope(
+      overrides: [
+        gameCardEvalWithStockfishFallbackProvider.overrideWith(
+          (ref, fen) async => _cloudEval(120),
+        ),
+        gameCardEvalCacheOnlyProvider.overrideWith(
+          (ref, fen) => cacheOnlyEval(),
+        ),
+      ],
+      child: MaterialApp(
+        home: Builder(
+          builder: (context) {
+            ResponsiveHelper.init(context);
+            return Scaffold(
+              body: EvaluationBarWidgetForGames(
+                width: 24,
+                height: 240,
+                fen: _fen,
+                playerView: PlayerView.listView,
+                allowStockfishFallback: allowStockfishFallback,
+              ),
+            );
+          },
+        ),
+      ),
+    ),
+  );
+}
+
+void main() {
+  testWidgets('retains previous eval while scroll cache-only eval is loading', (
+    tester,
+  ) async {
+    final pendingCacheOnly = Completer<CloudEval>();
+
+    await _pumpEvalBar(
+      tester,
+      allowStockfishFallback: true,
+      cacheOnlyEval: () => pendingCacheOnly.future,
+    );
+    await tester.pump();
+
+    expect(find.text('+1.2'), findsOneWidget);
+
+    await _pumpEvalBar(
+      tester,
+      allowStockfishFallback: false,
+      cacheOnlyEval: () => pendingCacheOnly.future,
+    );
+    await tester.pump();
+
+    expect(find.text('+1.2'), findsOneWidget);
+    expect(find.text('...'), findsNothing);
+  });
+}
