e2cff3ea0fa438d84642c8e22d2afecf79344d7d
dagidici <34388506+dagidici@users.noreply.github.com>
2026-08-31 15:04:32 -0500

    fix(phone): show the Book sign on book moves (34.8.23) (#318)
    Co-authored-by: Hermes Agent <hermes@nousresearch.com>

--- lib/screens/chessboard/chess_board_screen_new.dart
+++ lib/screens/chessboard/chess_board_screen_new.dart
@@ -8746,6 +8746,7 @@ class _AnalysisBoardState extends ConsumerState<_AnalysisBoard>
     required Square square,
     required LichessMoveAnnotation annotation,
   }) {
+    final badgeSize = widget.size / 8 * 0.40;
     return _buildBoardBadge(
       square: square,
       color: annotation.type.color,
@@ -8753,6 +8754,8 @@ class _AnalysisBoardState extends ConsumerState<_AnalysisBoard>
       selfContainedChild: true,
       child: SvgPicture.asset(
         annotation.type.iconAssetPath,
+        width: badgeSize,
+        height: badgeSize,
         fit: BoxFit.contain,
       ),
     );
